102a06f226b9730aaadb108ae4278c6cb0ab00c8
crissyrietti <cristinarietti@gmail.com>
2022-09-22 09:00:55 -0400

    commit again

--- Assignment 2.5/Debug.py
+++ Assignment 2.5/Debug.py
@@ -34,8 +34,8 @@ def main():
     numbers = [1,2,3,4,5]
     print(numbers[1])
     print(numbers[3])
-    print(numbers[4] / numbers[2])
-    print(numbers[4] * numbers[2])
+    print(numbers[3] / numbers[1])
+    print(numbers[3] * numbers[1])
     
     #again, why is it getting 7 and 20, I wanted 3 and 29!
     numbers2 = [465,3,30,7,29,20,82,13,5]
